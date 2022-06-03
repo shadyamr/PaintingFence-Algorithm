0e506188909ac01bebbeb415e7829fa129fe96ca
Ahmed Hossam <86417843+XIK9@users.noreply.github.com>
2022-06-03 16:04:44 +0200

    Create DynamicApproach.cpp

---
+++ DynamicApproach.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int PaintFenceDynamic(int NumberOfPosts, int NumberOfColors)
+{
+    if (NumberOfPosts == 0) 
+    {
+        return 0;
+    }
+    if (NumberOfPosts == 1)
+    {
+        return NumberOfPosts;
+    }
+    int same = NumberOfColors, diff = NumberOfColors * (NumberOfColors - 1);
+    for (int i = 3; i <= NumberOfPosts; i++)
+    {
+        int PrevDiff = diff;
+        diff = (same + diff) * (NumberOfColors - 1);
+        same = PrevDiff * 1;
+    }
+    return same + diff;
+}
+
+int main()
+{
+    cout << PaintFenceDynamic(2, 4) << endl;
+}
