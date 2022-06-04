d1d387db9d42120db2869d808157f19a56c00bd0
Seif Hisham <74967136+seifhisham@users.noreply.github.com>
2022-06-04 20:56:40 +0200

    Update BruteForceApproach.cpp

--- BruteForceApproach.cpp
+++ BruteForceApproach.cpp
@@ -1,7 +1,41 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-  cout << "To be updated!" << endl;
+string s;
+int n;
+int ans;
+void permute(int idx) {
+    if (idx == n) {
+        int cnt = 0;
+        for (int i = 0; i < n - 1; ++i) {
+            if (s[i] == s[i + 1]) {
+                cnt++;
+                if (cnt > 1)
+                    break;
+            }
+            else {
+                cnt = 0;
+            }
+        }
+        if (cnt <= 1) {
+            cout << s << endl;
+            ans++;
+        }
+        return;
+    }
+
+    for (char ch : {'A', 'B', 'C'}) {
+        s[idx] = ch;
+        permute(idx + 1);
+    }
+}
+
+
+int main() {
+    cin >> n;
+    s = "ABCD";
+    permute(0);
+    cout << ans;
+
+    return 0;
 }
