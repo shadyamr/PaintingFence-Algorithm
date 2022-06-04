73825f53cd85883fc588011a8b9de44aac6a2a87
Ahmed Hossam <86417843+AhmedHossam9@users.noreply.github.com>
2022-06-04 22:45:38 +0200

    Update G2G-DynamicApproach.cpp

--- G2G-DynamicApproach.cpp
+++ G2G-DynamicApproach.cpp
@@ -1,18 +1,36 @@
 #include <iostream>
 using namespace std;
+
+// Returns count of ways to color k posts
+// using k colors
 long countWays(int n, int k)
 {
+    // There are k ways to color first post
     long total = k;
     int mod = 1000000007;
+
+    // There are 0 ways for single post to
+    // violate (same color) and k ways to
+    // not violate (different color)
     int same = 0, diff = k;
+
+    // Fill for 2 posts onwards
     for (int i = 2; i <= n; i++) {
+        // Current same is same as previous diff
         same = diff;
+
+        // We always have k-1 choices for next post
         diff = total * (k - 1);
         diff = diff % mod;
+
+        // Total choices till i.
         total = (same + diff) % mod;
     }
+
     return total;
 }
+
+// Driver code
 int main()
 {
     int n =4 , k = 5;
