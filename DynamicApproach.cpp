113a0fd4d7d4ec1470c9df6ea7ddfa0bc88b7d06
Ahmed Hossam <86417843+AhmedHossam9@users.noreply.github.com>
2022-06-05 10:34:22 +0200

    Update DynamicApproach.cpp

--- DynamicApproach.cpp
+++ DynamicApproach.cpp
@@ -1,40 +1,27 @@
 #include <iostream>
 using namespace std;
 
-string s;
-int ans;
-
-void PaintFenceBF(int idx, int n){
-
-    if (idx == n) {
-        int cnt = 0;
-        for (int i = 0; i < n - 1; ++i){
-            if (s[i] == s[i + 1]){
-                cnt++;
-                if (cnt > 1) {
-                    break;
-                }
-            }
-            else{
-                cnt = 0;
-            }
-        }
-        if (cnt <= 1){
-            ans++;
-        }
-        return;
+int PaintFenceDynamic(int NumberOfPosts, int NumberOfColors)
+{
+    if (NumberOfPosts == 0)
+    {
+        return 0;
     }
-
-    for (char ch : {'A','B','C'}){
-        s[idx] = ch;
-        PaintFenceBF(idx + 1, n);
+    if (NumberOfPosts == 1)
+    {
+        return NumberOfPosts;
     }
+    int same = NumberOfColors, diff = NumberOfColors * (NumberOfColors - 1);
+    for (int i = 3; i <= NumberOfPosts; i++)
+    {
+        int PrevDiff = diff;
+        diff = (same + diff) * (NumberOfColors - 1);
+        same = PrevDiff * 1;
+    }
+    return same + diff;
 }
 
-int main(){
-    int n;
-    cin >> n;
-    s = "ABCD";
-    PaintFenceBF(0, n);
-    cout << "The result is: " << ans;
+int main()
+{
+    cout << "* Output: " << PaintFenceDynamic(4,3) << endl;
 }
